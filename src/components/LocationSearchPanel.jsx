3fad8dbbe242f23722f605e828d7164e1dbc2af0
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-12-07 14:31:08 +0500

    Initialized

--- src/components/LocationSearchPanel.jsx
+++ src/components/LocationSearchPanel.jsx
@@ -4,6 +4,9 @@ const LocationSearchPanel = ({ suggestions, setVehiclePanel, setPanelOpen, setPi
 
     const handleSuggestionClick = (suggestion) => {
         if (activeField === 'pickup') {
+            console.log("suggestion");
+            console.log(suggestion)
+            console.log(suggestions)
             setPickup(suggestion)
         } else if (activeField === 'destination') {
             setDestination(suggestion)
@@ -16,7 +19,10 @@ const LocationSearchPanel = ({ suggestions, setVehiclePanel, setPanelOpen, setPi
         <div>
             {/* Display fetched suggestions */}
             {
-                suggestions.map((elem, idx) => (
+                
+                
+                (suggestions||[]).map((elem, idx) => (
+                    
                     <div key={idx} onClick={() => handleSuggestionClick(elem)} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
                         <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
                         <h4 className='font-medium'>{elem}</h4>
